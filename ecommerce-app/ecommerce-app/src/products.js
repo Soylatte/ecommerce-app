f2671687e08fe3a0c56e0b2150510f908d2aa4cb
Soyeon Kim <kksoyeon61@gmail.com>
2024-01-31 16:21:18 +0100

    add via upload

---
+++ ecommerce-app/ecommerce-app/src/products.js
@@ -0,0 +1,66 @@
+import product1 from './assets/1.png'
+import product2 from './assets/2.png'
+import product3 from './assets/galxy.png'
+import product4 from './assets/iphone.png'
+import product5 from './assets/micro.jpg'
+import product6 from './assets/console.jpg'
+import product7 from './assets/pc.jpg'
+import product8 from './assets/macbook.jpg'
+import product9 from './assets/watch.jpg'
+import product10 from'./assets/ipad.jpg'
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "IPhone",
+        price:999,
+        productImage: product1,
+    },
+    {
+        id:2,
+        productName: "Macbook Pro2022(M1)",
+        price:1450,
+        productImage: product2,
+    },
+    {
+        id:3,
+        productName: "Galaxy Note5",
+        price:800,
+        productImage: product3,
+    },
+    {
+        id:4,
+        productName: "IPhone 5",
+        price:500,
+        productImage: product4,
+    },{
+        id:5,
+        productName: "Micro wave v.4",
+        price:400,
+        productImage: product5,
+    },{
+        id:6,
+        productName: "Console v.3",
+        price:500,
+        productImage: product6,
+    },{
+        id:7,
+        productName: "Galaxy book 2",
+        price:800,
+        productImage: product7,
+    },{
+        id:8,
+        productName: "MacBook 2023(M2) ",
+        price:1800,
+        productImage: product8,
+    },{
+        id:9,
+        productName: "Apple watch Hermes",
+        price:2000,
+        productImage: product9,
+    },{
+        id:10,
+        productName: "Ipad 10 air",
+        price:700,
+        productImage: product10,
+    },
+]
